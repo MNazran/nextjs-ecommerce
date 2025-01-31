32aac266b5a4026b04aa3bbbd70ee1cc1a51c583
MNazran <muhammadnazran78@gmail.com>
2025-01-31 11:44:47 +0800

    Implement edit product page and update ProductForm to handle existing product data

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -7,17 +7,26 @@ import { Textarea } from '@/components/ui/textarea';
 import { formatCurrency } from '@/lib/formatters';
 import { useActionState, useState } from 'react';
 import { addProduct } from '../../_actions/products';
-import { useFormState, useFormStatus } from 'react-dom';
+import { useFormStatus } from 'react-dom';
+import { Product } from '@prisma/client';
 
-export function ProductForm() {
+export function ProductForm({ product }: { product?: Product | null }) {
   const [error, action] = useActionState(addProduct, {});
-  const [priceInCents, setPriceInCents] = useState<number | string>('');
+  const [priceInCents, setPriceInCents] = useState<number | undefined>(
+    product?.priceInCents
+  );
 
   return (
     <form action={action} className='space-y-8'>
       <div className='space-y-2'>
         <Label htmlFor='name'>Name</Label>
-        <Input type='text' id='name' name='name' required />
+        <Input
+          type='text'
+          id='name'
+          name='name'
+          required
+          defaultValue={product?.name || ''}
+        />
         {error.name && <div className='text-destructive'>{error.name}</div>}
       </div>
       <div className='space-y-2'>
@@ -28,7 +37,7 @@ export function ProductForm() {
           name='priceInCents'
           required
           value={priceInCents}
-          onChange={(e) => setPriceInCents(e.target.value)}
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
         />
         <div className='text-muted-foreground'>
           {formatCurrency((Number(priceInCents) || 0) / 100)}
@@ -39,7 +48,12 @@ export function ProductForm() {
 
         <div className='space-y-2'>
           <Label htmlFor='desc'>Description</Label>
-          <Textarea id='description' name='description' required />
+          <Textarea
+            id='description'
+            name='description'
+            required
+            defaultValue={product?.description || ''}
+          />
           {error.description && (
             <div className='text-destructive'>{error.description}</div>
           )}
@@ -47,12 +61,12 @@ export function ProductForm() {
       </div>
       <div className='space-y-2'>
         <Label htmlFor='name'>File</Label>
-        <Input type='file' id='file' name='file' required />
+        <Input type='file' id='file' name='file' required={product == null} />
         {error.file && <div className='text-destructive'>{error.file}</div>}
       </div>
       <div className='space-y-2'>
         <Label htmlFor='image'>Image</Label>
-        <Input type='file' id='image' name='image' required />
+        <Input type='file' id='image' name='image' required={product == null} />
         {error.image && <div className='text-destructive'>{error.image}</div>}
       </div>
       <SubmitButton />
