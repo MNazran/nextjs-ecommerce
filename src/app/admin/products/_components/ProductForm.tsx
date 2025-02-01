5796292f3062bcaa28ce5ce684b869bd608c55c7
MNazran <muhammadnazran78@gmail.com>
2025-02-01 08:08:43 +0800

    Introduce product update functionality with file and image handling in admin panel

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -9,6 +9,7 @@ import { useActionState, useState } from 'react';
 import { addProduct } from '../../_actions/products';
 import { useFormStatus } from 'react-dom';
 import { Product } from '@prisma/client';
+import Image from 'next/image';
 
 export function ProductForm({ product }: { product?: Product | null }) {
   const [error, action] = useActionState(addProduct, {});
@@ -62,11 +63,22 @@ export function ProductForm({ product }: { product?: Product | null }) {
       <div className='space-y-2'>
         <Label htmlFor='name'>File</Label>
         <Input type='file' id='file' name='file' required={product == null} />
+        {product != null && (
+          <div className='text-muted-foreground'>{product.filePath}</div>
+        )}
         {error.file && <div className='text-destructive'>{error.file}</div>}
       </div>
       <div className='space-y-2'>
         <Label htmlFor='image'>Image</Label>
         <Input type='file' id='image' name='image' required={product == null} />
+        {product != null && (
+          <Image
+            src={product.imagePath}
+            height='400'
+            width='400'
+            alt='Product Image'
+          />
+        )}
         {error.image && <div className='text-destructive'>{error.image}</div>}
       </div>
       <SubmitButton />
