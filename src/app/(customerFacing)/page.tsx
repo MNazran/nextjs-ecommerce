7d9cc0e68cc43039395fb06d0db85300242c72e7
MNazran <muhammadnazran78@gmail.com>
2025-02-07 03:45:24 +0800

    Add product grid sections for most popular and newest products on the home page

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,3 +1,48 @@
+import db from '@/db/db';
+import { Product } from '@prisma/client';
+
+function getMostPopularProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { orders: { _count: 'desc' } },
+    take: 6,
+  });
+}
+
+function getNewestProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { createdAt: 'desc' },
+    take: 6,
+  });
+}
+
 export default function HomePage() {
-  return <h1>Hi World</h1>;
+  return (
+    <main className='space-y-12'>
+      <ProductGridSection
+        title='Most Popular'
+        productsFetcher={getMostPopularProducts}
+      />
+      <ProductGridSection title='Newest' productsFetcher={getNewestProducts} />
+    </main>
+  );
+}
+
+type ProductGridSectionProps = {
+  title: string;
+  productsFetcher: () => Promise<Product[]>; // This is a function that returns a promise
+};
+
+function ProductGridSection({
+  productsFetcher,
+  title,
+}: ProductGridSectionProps) {
+  return (
+    <div className='space-y-4'>
+      <div className='flex gap-4'>
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
 }
