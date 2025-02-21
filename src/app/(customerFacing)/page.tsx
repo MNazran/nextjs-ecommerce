2f25f206256b8c8e33b61a301144d0294aa7429c
MNazran <muhammadnazran78@gmail.com>
2025-02-21 19:57:10 +0800

    Add ProductCard component and enhance product grid section with navigation

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,5 +1,9 @@
+import { ProductCard } from '@/components/ProductCard';
+import { Button } from '@/components/ui/button';
 import db from '@/db/db';
 import { Product } from '@prisma/client';
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
 
 function getMostPopularProducts() {
   return db.product.findMany({
@@ -41,7 +45,16 @@ function ProductGridSection({
   return (
     <div className='space-y-4'>
       <div className='flex gap-4'>
-        <h2>{title}</h2>
+        <h2 className='text-3xl font-bold'>{title}</h2>
+        <Button variant='outline' asChild>
+          <Link href='/products' className='space-x-2'>
+            <span>View All</span>
+            <ArrowRight className='size-4' />
+          </Link>
+        </Button>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        <ProductCard />
       </div>
     </div>
   );
