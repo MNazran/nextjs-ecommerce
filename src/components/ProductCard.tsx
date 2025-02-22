f791b9fe7ffb5226c9204dc998776c9b72466210
MNazran <muhammadnazran78@gmail.com>
2025-02-22 22:40:44 +0800

    Extend ProductCard to include detailed info and a purchase link

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,42 @@
-import { Card } from './ui/card';
+import { formatCurrency } from '@/lib/formatters';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+import { Button } from './ui/button';
+import Link from 'next/link';
 
-export function ProductCard() {
-  return <Card></Card>;
+interface ProductCardProps {
+  name: string;
+  priceInCents: number;
+  description: string;
+  id: number;
+}
+
+export function ProductCard({
+  name,
+  priceInCents,
+  description,
+  id,
+}: ProductCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>{formatCurrency(priceInCents / 100)}</CardDescription>
+      </CardHeader>
+      <CardContent className='flex-grow'>
+        <p className='line-clamp-4'>{description}</p>
+      </CardContent>
+      <CardFooter>
+        <Button asChild size='lg' className='w-full'>
+          <Link href={`/products/${id}/purchase`}></Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 }
