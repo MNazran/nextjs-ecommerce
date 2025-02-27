29bdc9583f98bdf7898bf981c8d78a2fd044f8e3
MNazran <muhammadnazran78@gmail.com>
2025-02-27 10:01:19 +0800

    Add ProductCardSkeleton for loading state in ProductGridSection

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,9 +1,10 @@
-import { ProductCard } from '@/components/ProductCard';
+import { ProductCard, ProductCardSkeleton } from '@/components/ProductCard';
 import { Button } from '@/components/ui/button';
 import db from '@/db/db';
 import { Product } from '@prisma/client';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 function getMostPopularProducts() {
   return db.product.findMany({
@@ -20,6 +21,15 @@ function getNewestProducts() {
     take: 6,
   });
 }
+
+// async function wait(duration: number) {
+//   await wait(2000);
+//   return db.product.findMany({
+//     where: { isAvailableForPurchase: true },
+//     orderBy: { createdAt: 'desc' },
+//     take: 6,
+//   });
+// }
 
 export default function HomePage() {
   return (
@@ -38,7 +48,7 @@ type ProductGridSectionProps = {
   productsFetcher: () => Promise<Product[]>; //a function that returns a promise
 };
 
-async function ProductGridSection({
+function ProductGridSection({
   productsFetcher,
   title,
 }: ProductGridSectionProps) {
@@ -54,10 +64,26 @@ async function ProductGridSection({
         </Button>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-        {(await productsFetcher()).map((product) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
+        <Suspense
+          fallback={
+            <>
+              <ProductCardSkeleton />
+            </>
+          }
+        >
+          <ProductSuspense productsFetcher={productsFetcher} />
+        </Suspense>
       </div>
     </div>
   );
+}
+
+async function ProductSuspense({
+  productsFetcher,
+}: {
+  productsFetcher: () => Promise<Product[]>;
+}) {
+  return (await productsFetcher()).map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
 }
