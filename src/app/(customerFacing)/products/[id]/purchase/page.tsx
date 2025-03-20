5183ce994240e0a9f8c68a3687c9b4c0353ae08f
MNazran <muhammadnazran78@gmail.com>
2025-03-21 06:58:04 +0800

    Include CheckoutForm component and integrate payment intent creation in PurchasePage

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,6 +1,7 @@
 import db from '@/db/db';
 import { notFound } from 'next/navigation';
 import Stripe from 'stripe';
+import { CheckoutForm } from './_components/CheckoutForm';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
@@ -12,5 +13,21 @@ export default async function PurchasePage({
   const product = await db.product.findUnique({ where: { id } });
 
   if (product == null) return notFound();
-  return <h1>sup!</h1>;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: 'MYR',
+    metadata: { productId: product.id },
+  });
+
+  if (paymentIntent.client_secret == null) {
+    throw Error('Stripe failed to create payment intent');
+  }
+
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
+  );
 }
